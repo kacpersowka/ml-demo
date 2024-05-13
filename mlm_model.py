00799a4745d5c20e73c7f162d04dad38c97a332c
Kacper Sowka <kacpersowka@gmail.com>
2024-05-13 12:56:05 +0100

    First commit, added two basic functions for interfacing masked language modelling functionality of the Transformers library BERT encoder.

---
+++ mlm_model.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer,AutoModelForMaskedLM
+import torch
+
+model = AutoModelForMaskedLM.from_pretrained('bert-base-uncased')
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+def get_masked_guesses(text,model):
+    tokens=tokenizer(text,return_tensors='pt')
+    masks=[i for i in range(len(tokens['input_ids'][0])) if tokens['input_ids'][0][i]==103]
+    outputs = model(**tokens)
+    mask_guesses=dict()
+    for i in masks:
+        mask_guesses[i]=sorted([(_,float(outputs.logits[0][i][_])) for _ in range(len(outputs.logits[0][i]))],key=lambda x:-x[1])
+    return mask_guesses
+
+def show_guess(text,guesses,index=0):
+    tokens=tokenizer(text)
+    masks=[i for i in range(len(tokens['input_ids'])) if tokens['input_ids'][i]==103]
+    for i in masks:
+        tokens['input_ids'][i]=guesses[i][index][0]
+    return tokenizer.decode(tokens['input_ids'])
